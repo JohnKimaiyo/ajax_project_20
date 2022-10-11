38a061e10e6b83af86c0f89a19539bee73f34b1c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-10-11 13:50:53 +0300

    setting up get request event listener

---
+++ app.js
@@ -0,0 +1,9 @@
+// gett button
+let getButton = document.querrySelector('#get-btn');
+getButton.addEventListener('click', function () {
+    fetchEmployees();
+});
+
+let fetchEmployees = () => {
+
+}
